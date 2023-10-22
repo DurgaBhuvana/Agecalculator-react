4c8770b8545f03d99398471fc3c7ec2a9fc64d72
DurgaBhuvana <mellacheruvudb@gmail.com>
2023-10-22 23:58:56 +0530

    first commit

---
+++ reactcalculator/src/components/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./footer.css";
+export default function Footer() {
+  var stringCode = "</>";
+  return (
+    <footer>
+      <p>
+        {" "}
+        {stringCode} by:{" "}
+        <a href="https://mohamadhossein.ir/">MohamadHossein.ir</a>{" "}
+      </p>
+      <p>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://github.com/M-Hosseini80/calculator"
+        >
+          github
+        </a>
+      </p>
+    </footer>
+  );
+}
